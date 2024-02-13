24a6a5914d2666428e8c146cb37cede4bcac92b1
David GAILLETON <david@RASKxin5DlrLG39.ras-interim.local>
2024-02-13 09:06:57 +0100

    First commit

---
+++ views/login.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GSB: Login</title>
+    <link rel="stylesheet" href="../public/style/style.css">
+</head>
+<body>
+    <main>
+        <div class="login_card">
+            <div class="login_content">
+                <h2>Login</h2>
+                <form action="../index.php" method="post" id="login">
+                    <input type="text" name="username" class="text_box">
+                    <input type="password" name="password" class="text_box">
+                    <input type="submit" value="Se connecter">
+                </form>
+            </div>
+        </div>
+
+    </main>
+</body>
+</html>
