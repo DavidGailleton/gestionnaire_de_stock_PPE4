d53d377395e69ad51ad595a094e81ba94b48385c
David GAILLETON <david@RASKxin5DlrLG39.ras-interim.local>
2024-02-23 19:51:42 +0100

    Ajout de la classe Log_connexion

---
+++ app/models/Log_connexion.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ppe4;
+
+use PDO;
+use ppe4\Model;
+
+class Log_connexion extends Model
+{
+    private \DateTime $date;
+    private string $email;
+    private bool $echec;
+
+    public function insert_log_connexion(string $email, bool $echec):void
+    {
+        $query = "INSERT INTO log_connexion (email_log_con, echec_log_con) VALUES (:email, :echec)";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute(['email' => $email], ['echec' => $echec]);
+    }
+
+    public function select_logs_utilisateur(string $email):array
+    {
+        $query = "SELECT id_log_con AS id, date_log_con AS date, email_log_con AS email, echec_log_con AS echec FROM log_connexion WHERE email_log_con = :email ORDER BY date_log_con DESC";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute(['email' => $email]);
+        return $stmt->fetchAll(PDO::FETCH_CLASS, '\ppe4\Log_connexion');
+    }
+
+    public function setLog_connexion(int $id, \DateTime $date, string $email, bool $echec):void
+    {
+        $this->date = $date;
+        $this->email = $email;
+        $this->echec = $echec;
+    }
+
+    public function getDate(): \DateTime
+    {
+        return $this->date;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function isEchec(): bool
+    {
+        return $this->echec;
+    }
+
+}
