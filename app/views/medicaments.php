0e7feb5b26412d7b4c1aea3abe9debe0c56c12b9
David GAILLETON <david@RASKxin5DlrLG39.ras-interim.local>
2024-02-28 13:08:47 +0100

    Boutons de choix de page en bas de page terminé

--- app/views/medicaments.php
+++ app/views/medicaments.php
@@ -1,6 +1,6 @@
 <?php
 $no_page = $_GET['no_page'];
-$nb_page = 0.0;
+$nb_page = 0;
 if (isset($_GET['recherche'])){
     $recherche = $_GET['recherche'];
 }
@@ -31,10 +31,10 @@ if (isset($_GET['recherche'])){
     $medicament = new \ppe4\Medicament();
     if (isset($recherche)){
         $medicaments = $medicament->select_medicaments_par_recherche(($no_page - 1) * 25, $recherche);
-        $nb_page = round($medicament->count_nb_medicament_par_recherche($recherche) / 25, 0, PHP_ROUND_HALF_EVEN);
+        $nb_page = intval(round($medicament->count_nb_medicament_par_recherche($recherche) / 25, 0, PHP_ROUND_HALF_EVEN));
     } else {
         $medicaments = $medicament->select_medicaments(($no_page - 1) * 25);
-        $nb_page = round($medicament->count_nb_medicament() / 25, 0, PHP_ROUND_HALF_EVEN);
+        $nb_page = intval(round($medicament->count_nb_medicament() / 25, 0, PHP_ROUND_HALF_EVEN));
     }
 
     include_once ROOT.'app/views/component/medic_card.php';
@@ -45,36 +45,36 @@ if (isset($_GET['recherche'])){
     }
     ?>
 
-    <div class="choix_de_page">
+    <div class="choix_de_page" style="grid-template-columns: <?php for($i=0; $i < $nb_page && $i < 11; $i++) echo '1fr ' ?>">
         <?php
         if ($no_page != 1 && $nb_page > 9) {
-            echo '<button class=""><</button>';
+            echo '<button onclick="redirect('.$no_page - 1 .')"><</button>';
         }
-        echo '<button>1</button>';
+        echo '<button onclick="redirect(1)">1</button>';
         if ($nb_page > 1){
             if ($nb_page < 10 || $no_page < 4 || $no_page > $nb_page - 3){
-                echo '<button>2</button>';
+                echo '<button onclick="redirect(2)">2</button>';
             } else {
               echo '<button>...</button>';
             }
         }
         if ($nb_page  > 2) {
             if ($nb_page < 10 || $no_page > $nb_page - 3 || $no_page < 4){
-                echo '<button>3</button>';
+                echo '<button onclick="redirect(3)">3</button>';
             }else{
-                echo '<button>'.$no_page - 2 .'</button>';
+                echo '<button onclick="redirect('.$no_page - 2 .')">'.$no_page - 2 .'</button>';
             }
         }
         if ($nb_page > 3){
             if ($nb_page < 10 || $no_page > $nb_page - 3 || $no_page < 4){
-                echo '<button>4</button>';
+                echo '<button onclick="redirect(4)">4</button>';
             } else {
-                echo '<button>'. $no_page - 1 .'</button>';
+                echo '<button onclick="redirect('.$no_page - 1 .')">'. $no_page - 1 .'</button>';
             }
         }
         if ($nb_page > 4){
             if ($nb_page < 10){
-                echo '<button>5</button>';
+                echo '<button onclick="redirect(5)">5</button>';
             } else if ($no_page < 4 || $no_page > $nb_page - 3){
                 echo '<button>...</button>';
             } else {
@@ -83,39 +83,50 @@ if (isset($_GET['recherche'])){
         }
         if ($nb_page > 5){
             if ($nb_page < 10){
-                echo '<button>6</button>';
+                echo '<button onclick="redirect(6)">6</button>';
             } else if ($no_page < 4 || $no_page > $nb_page - 3){
-                echo '<button>'.$nb_page - 3 .'</button>';
+                echo '<button onclick="redirect('.$nb_page - 3 .')">'.$nb_page - 3 .'</button>';
             } else {
-                echo '<button>'. $no_page + 1 .'</button>';
+                echo '<button onclick="redirect('.$no_page + 1 .')">'. $no_page + 1 .'</button>';
             }
         }
         if ($nb_page > 6){
             if ($nb_page < 10){
-                echo '<button>7</button>';
+                echo '<button onclick="redirect(7)">7</button>';
             } else if ($no_page < 4 || $no_page > $nb_page - 3){
-                echo '<button>'.$nb_page - 2 .'</button>';
+                echo '<button onclick="redirect('.$nb_page - 2 .')">'.$nb_page - 2 .'</button>';
             } else {
-                echo '<button>'. $no_page + 2 .'</button>';
+                echo '<button onclick="redirect('.$no_page + 2 .')">'. $no_page + 2 .'</button>';
             }
         }
         if ($nb_page > 7){
             if ($nb_page < 10){
-                echo '<button>8</button>';
+                echo '<button onclick="redirect(8)">8</button>';
             } else if ($no_page < 4 || $no_page > $nb_page - 3){
-                echo '<button>'.$nb_page - 1 .'</button>';
+                echo '<button onclick="redirect('.$nb_page - 1 .')">'.$nb_page - 1 .'</button>';
             } else {
                 echo '<button>...</button>';
             }
         }
         if ($nb_page > 8){
-            echo '<button>'. $nb_page .'</button>';
+            echo '<button onclick="redirect('.$nb_page.')">'. $nb_page .'</button>';
         }
         if ($nb_page > 9 && $no_page != $nb_page){
-            echo '<button>></button>';
+            echo '<button onclick="redirect('.$no_page + 1 .')">></button>';
         }
         ?>
     </div>
+    <script>
+        function redirect (no_page){
+            <?php
+            if (isset($_GET['recherche'])){
+                echo 'window.location.href = "'.SERVER_URL.'index.php?page=medicaments&recherche='.$_GET['recherche'].'&no_page="+no_page';
+            }else{
+                echo 'window.location.href = "'.SERVER_URL.'index.php?page=medicaments&no_page="+no_page';
+            }
+            ?>
+        }
+    </script>
 </main>
 <?php include_once ROOT.'app/views/component/footer.php'?>
 </body>
