bc20fe01312366240bf63960933a2c39ac455b7c
David GAILLETON <david@RASKxin5DlrLG39.ras-interim.local>
2024-03-19 10:03:45 +0100

    Login card CSS

--- app/views/login.php
+++ app/views/login.php
@@ -4,17 +4,23 @@
     <?php require_once (ROOT."./app/views/component/head.php") ?>
 </head>
 <body>
+    <div>
+
+    </div>
     <main>
         <div class="login_card">
             <div class="login_content">
                 <h2>Login</h2>
                 <form action="index.php?action=login" method="post" id="login">
-                    <input type="text" name="email" class="text_box">
-                    <input type="password" name="password" class="text_box">
+                    <input type="email" name="email" class="text_box" placeholder="Addresse e-mail">
+                    <input type="password" name="password" class="text_box" placeholder="Mot de passe">
                     <input type="submit" value="connect">
                 </form>
             </div>
         </div>
     </main>
+    <footer>
+        Copyright GSB
+    </footer>
 </body>
 </html>
